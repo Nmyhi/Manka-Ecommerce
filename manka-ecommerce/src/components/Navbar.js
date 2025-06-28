3e9d78148a39d116eb894620362f85d8cf84cea6
Mihaly Lovrencsics <lomysa@gmail.com>
2025-06-28 17:19:37 +0000

    Add logo, font: Quicksand

--- manka-ecommerce/src/components/Navbar.js
+++ manka-ecommerce/src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../logo.png';  // Correct relative import
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <h1 className="navbar-title">Anya's Crochet</h1>
+        <Link to="/">
+          <img src={logo} alt="Manka Logo" className="navbar-logo" />
+        </Link>
         <div className="navbar-links">
-          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/" className="navbar-link">Shop</Link>
           <Link to="/cart" className="navbar-link">Cart</Link>
           <Link to="/checkout" className="navbar-link">Checkout</Link>
         </div>
